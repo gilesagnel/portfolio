533f4ee212c33023b6c27da5a5d4a51804446597
gilesagnel <gilesagnel@gmail.com>
2025-01-25 20:06:32 -0600

    Robotic software engineer

--- src/components/links.vue
+++ src/components/links.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex space-x-10 2xl:text-4xl text-3xl slit-in-diagonal-1">
+  <div class="flex space-x-16 2xl:text-4xl text-3xl slit-in-diagonal-1 mt-6 justify-center">
     <div>
       <a href="https://www.linkedin.com/in/agnel-giles-627340112/" target="_blank"
-        ><i class="fab fa-linkedin text-white hover:text-greenBlue"></i
+        ><i class="fab fa-linkedin text-white hover:text-greenBlue text-4xl"></i
       ></a>
     </div>
     <div>
       <a href="https://github.com/gilesagnel" target="_blank"
-        ><i class="fab fa-github-square text-white  hover:text-greenBlue"></i
+        ><i class="fab fa-github-square text-white  hover:text-greenBlue text-4xl"></i
       ></a>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,59 @@
 <template>
   <div>
-    <section id="home" class="flex flex-col-reverse md:flex-row justify-left xl:space-x-54 lg:space-x-34 sm:space-x-12 xl:p-8  mx-auto mb-10">
-      <div class="w-2/3 flex flex-col space-y-4 pr-60">
-        <div class="mt-4 xl:mt-0">
-          <h2 class="3xl:text-2xl text-greenBlue">
-            <span class="text-white lg:text-5xl tracking-normal font-bold">{{$t('myName')}}</span>
-          </h2>
+    <section id="home"
+      class="flex flex-col-reverse md:flex-row justify-left xl:space-x-54 lg:space-x-34 sm:space-x-12 xl:p-8  mx-auto mb-10">
+      <div class="w-100% flex m-12 mt-4 p-12 rounded-md bg-jet shadow-greenBlue">
+        <div class="w-2/3 flex flex-col space-y-4 pr-60">
+          <div class="mt-4 xl:mt-0">
+            <h2 class="3xl:text-2xl text-flashWhite">
+              <span class="text-flashWhite lg:text-5xl tracking-normal font-bold">{{ $t('myName') }}</span>
+            </h2>
+          </div>
+          <div>
+            <h3 class="text-flashWhite text-opacity-80 text-3xl 2xl:text-6xl tracking-normal focus-in-expand ">{{ $t('job') }}</h3>
+          </div>
+          <div>
+            <h5 class="text-flashWhite text-opacity-70 text-xl 2xl:text-3xl tracking-normal focus-in-expand ">{{ $t('education') }}</h5>
+          </div>
+          <div>
+            <p
+              class="text-sm lg:text-base leading-loose tracking-relaxed lg:leading-loose lg:tracking-relaxed text-flashWhite text-opacity-60 whitespace-pre-line">
+              Location: {{ $t('location') }}</p>
+          </div>
+          <div>
+            <p
+              class="text-sm lg:text-base leading-loose tracking-relaxed lg:leading-loose lg:tracking-relaxed text-flashWhite text-opacity-50 whitespace-pre-line">
+              {{ $t('Aboutme') }}</p>
+          </div>
         </div>
-        <div>
-          <h3 class="text-greenBlue text-3xl 2xl:text-6xl tracking-normal focus-in-expand ">{{ $t('job') }}</h3>
-        </div>
-        <div>
-          <p class="text-sm lg:text-base leading-loose tracking-relaxed lg:leading-loose lg:tracking-relaxed text-slateGray whitespace-pre-line" >{{$t('Aboutme')}}</p>
-        </div>
-        
-        <!-- See my resume -->
-        <a href="https://drive.google.com/file/d/15yHJjoG8r2xnRB1i7hS9qlHPhuvbWs_2/view?usp=sharing" target="_blank" class="text-xs text-white lg:text-base bg-transparent w-full 2xl:w-1/3 hover:bg-greenBlue font-semibold uppercase text-center hover:text-white py-2 px-8 border border-white hover:border-transparent rounded">
-          {{$t('seeResume')}}
-        </a>
+        <div class="w-2/5 h-100% flex flex-col">
+          <div class="flex w-100% h-3/5 m-4">
+            <img class="rounded-3xl" src="../assets/agnel.jpg" alt="Agnel" />
+          </div>
+          <div class="flex flex flex-col mt-8">
+            <a href="https://drive.google.com/file/d/15yHJjoG8r2xnRB1i7hS9qlHPhuvbWs_2/view?usp=sharing" target="_blank"
+              class="text-xs text-flashWhite lg:text-base bg-transparent 2xl:w-1/3 hover:bg-greenBlue font-semibold uppercase text-center hover:text-flashWhite py-2 px-8 border border-white hover:border-transparent rounded mx-20">
+              {{ $t('seeResume') }}
+            </a>
 
-        <!-- Social media icons -->
-        <v-links/>
-        
-      </div>
-      <div class="w-1/3">
-        <img class="2xl:w-2/3 slit-in-diagonal-1 rounded-lg" src="../assets/agnel.jpg" alt="Agnel"/>
+            <!-- Social media icons -->
+            <v-links />
+          </div>
+        </div>
       </div>
     </section>
 
 
     <!-- Skills Section -->
     <section id="skills" class="my-32">
-      <Skills/>
+      <Skills />
     </section>
 
-    <section id="timeline" class="my-32" >
+    <section id="timeline" class="my-32">
       <Timeline />
     </section>
 
-    <section id="projects" class="my-32" >
+    <section id="projects" class="my-32">
       <Projects />
     </section>
 
@@ -47,7 +62,7 @@
 
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from 'vuex'
 import VLinks from '../components/links.vue'
 import Skills from '../components/Skills.vue'
 import Timeline from '../components/Timeline.vue';
@@ -55,80 +70,80 @@ import Projects from '../components/Projects.vue';
 
 export default {
   name: "Home",
-  components: {VLinks, Skills, Timeline, Projects},
+  components: { VLinks, Skills, Timeline, Projects },
   data() {
     return {
       dataTimeline: [
-      {
-        from: new Date(2017, 5, 22),
-        title: "Name",
-        showDayAndMonth: true,
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      },
-      {
-        from: new Date(2017, 8),
-        title: "Name",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      },
-      {
-        from: new Date(2016, 11),
-        title: "Name",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      },
-      {
-        from: new Date(2018, 7, 19),
-        title: "Name",
-        showDayAndMonth: true,
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      },
-      {
-        from: new Date(2016, 1),
-        title: "Name",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      },
-      {
-        from: new Date(2016, 6),
-        title: "Name",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      },
-      {
-        from: new Date(2013, 1),
-        title: "Name",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      },
-      {
-        from: new Date(2015, 1),
-        title: "Name",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      },
-      {
-        from: new Date(2012, 1),
-        title: "Name",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
-      }]
+        {
+          from: new Date(2017, 5, 22),
+          title: "Name",
+          showDayAndMonth: true,
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        },
+        {
+          from: new Date(2017, 8),
+          title: "Name",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        },
+        {
+          from: new Date(2016, 11),
+          title: "Name",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        },
+        {
+          from: new Date(2018, 7, 19),
+          title: "Name",
+          showDayAndMonth: true,
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        },
+        {
+          from: new Date(2016, 1),
+          title: "Name",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        },
+        {
+          from: new Date(2016, 6),
+          title: "Name",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        },
+        {
+          from: new Date(2013, 1),
+          title: "Name",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        },
+        {
+          from: new Date(2015, 1),
+          title: "Name",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        },
+        {
+          from: new Date(2012, 1),
+          title: "Name",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+        }]
     };
   },
-  computed:{
-    ...mapGetters({isTranslated: 'isTranslated'})
+  computed: {
+    ...mapGetters({ isTranslated: 'isTranslated' })
   },
-  methods:{
-    goToResume(){
+  methods: {
+    goToResume() {
       this.$router.push({ name: 'Resume' })
     }
   },
-  mounted(){
+  mounted() {
     window.addEventListener('scroll', this.$refs.scrollTop.scrollListener)
   },
-  destroyed(){
+  destroyed() {
     this.$store.commit('ToggleIsTranslated')
   }
 };
@@ -140,6 +155,7 @@ export default {
   content: "|";
   animation: blink 1s infinite step-start;
 }
+
 @keyframes blink {
   50% {
     opacity: 0;
@@ -152,11 +168,13 @@ export default {
     animation-timing-function: ease-in;
     opacity: 0;
   }
+
   54% {
     transform: translateZ(-160px) rotate3d(1, 1, 0, 87deg);
     animation-timing-function: ease-in-out;
     opacity: 1;
   }
+
   100% {
     transform: translateZ(0) rotate3d(1, 1, 0, 0);
     animation-timing-function: ease-out;
@@ -169,6 +187,7 @@ export default {
     filter: blur(12px);
     opacity: 0;
   }
+
   100% {
     filter: blur(0);
     opacity: 1;
@@ -182,5 +201,10 @@ export default {
 
 .focus-in-expand {
   animation: focus-in-expand 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+}
+
+.shadow-greenBlue {
+  --tw-shadow: 0 10px 36px -3px rgb(54 115 219), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  box-shadow: var(--tw-ring-offset-shadow, 0 0 rgb(54 115 219)), var(--tw-ring-shadow, 0 0 rgb(54 115 219)), var(--tw-shadow);
 }
 </style>../components/Timeline.vue
